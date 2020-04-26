ff83982fee1ac84763284240760c611dc5f93d58
Evgeniy Popov <PNDSpirit@gmail.com>
2020-04-26 18:25:06 +0300

    add arguments

--- typist.py
+++ typist.py
@@ -8,13 +8,13 @@ import threading
 
 
 class Typist:
-    def __init__(self):
+    def __init__(self, wpm, accuracy=100, error_deviation=0):
         self.characters_queue = queue.SimpleQueue()
-        self.accuracy = 100
-        self.error_deviation = 0.2
-        self.wpm = 165
+        self.accuracy = accuracy
+        self.error_deviation = error_deviation
+        self.wpm = wpm
         self.cpm = self.wpm * 5
-        self.characters_written = 0
+
         keyboard.send(0)
         threading.Thread(target=self._type_loop).start()
 
@@ -23,29 +23,59 @@ class Typist:
             try:
                 char = self.characters_queue.get(timeout=5)
             except queue.Empty:
-                print("Queue is empty")
                 return
+
             if char is not None:
                 self._press_a_key(char)
+                self._make_a_mistake(char)
 
-            self._make_a_mistake()
 
-    def _make_a_mistake(self):
+    def _make_a_mistake(self, char):
         pool_size = 10000
         chance = (100 - self.accuracy) / 100
         roll = random.randint(0, pool_size - 1)
 
         if roll < pool_size * chance:
             # write and delete a char
-            self._press_a_key("0")
+            self._press_a_key(char)
             self._press_a_key("backspace")
 
+    def get_letter_in_good_form(self, letter):
+        shifted_keys = {
+            "!": "1",
+            "@": "2",
+            "#": "3",
+            "$": "4",
+            "%": "5",
+            "^": "6",
+            "&": "7",
+            "*": "8",
+            "(": "9",
+            ")": "0",
+            ":": ";",
+            "\"": "'",
+            "{": "[",
+            "}": "]",
+            "+": "=",
+            "_": "-",
+            "<": ",",
+            ">": ".",
+            "?": "/",
+            "~": "`",
+        }
+        
+        if len(letter) is 1:
+            if letter.isupper():
+                letter = "shift+" + letter.lower()
+            if letter in shifted_keys.keys():
+                letter = "shift+" + shifted_keys[letter]
+
+        return letter
+
     def _press_a_key(self, letter):
-        if len(letter) is 1 and letter.isupper():
-            letter = "shift+" + letter.lower()
+        letter = self.get_letter_in_good_form(letter)
         keyboard.send(letter)
         time.sleep(self._get_sleep_time())
-        print("working")
 
     def _get_sleep_time(self, char=None):
         roll = random.randint(-100, 100) / 100
@@ -57,9 +87,19 @@ class Typist:
         for char in chars:
             self.characters_queue.put(char)
 
+    def _get_error_letter(self, char):
+        if char.isalnum():
+            if len(char) is 1:
+                return "shift+" + char
+            else:
+                return char[-1]
+        return char
+
+    def stop_typing(self):
+        self.characters_queue = queue.SimpleQueue()
+
 
 if __name__ == "__main__":
-    # time.sleep(5)
+    time.sleep(1)
     typist = Typist()
-    typist.insert_characters("hi")
     typist.insert_characters("hi")
