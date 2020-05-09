67cac68add16f5692dc0b8014bcd2a55e9cffea2
Evgeniy Popov <PNDSpirit@gmail.com>
2020-05-09 21:15:54 +0300

    files

---
+++ config.py
@@ -0,0 +1,21 @@
+screenshots_folder = "screenshots/"
+dotted_line = "pictures/dotted_line_150_zoom.png"
+change_display_format = "pictures/change_display_format_150_zoom.png"
+yellow_light = "pictures/yellow_light_150_zoom.png"
+main_page = "pictures/main_page_150_zoom.png"
+# dotted_line = "pictures/dotted_line.png"
+box_width_left = 380  # 150% zoom
+box_width_right = 1325  # 150% zoom
+mouse_offset_of_change_display_typebox = 55  # 150% zoom
+mouse_width_typebox = 666
+wpm = 135
+accuracy = 97.6
+error_deviation = 0.4
+bad_keys = "“|’‘|’‘§\\”"
+with open("bad_keys") as f:
+    for line in f:
+        bad_keys += line
+
+yellow_light_position = (660, 299, 969, 419)
+main_page_picture_position = (370, 272, 495, 398)
+
